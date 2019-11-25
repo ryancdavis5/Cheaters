366b9d1c64b1e328827a1ece635f3f05a2af69ff
ryancdavis5 <47760457+ryancdavis5@users.noreply.github.com>
2019-11-25 16:23:01 -0600

    printing chunks

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <queue>
 
 using namespace std;
 int getdir (string dir, vector<string> &files){
@@ -21,6 +23,49 @@ int getdir (string dir, vector<string> &files){
     closedir(dp);
     return 0;
 }
+void printQ(queue<string> myqueue){
+    while (!myqueue.empty()) {
+        cout << ' ' << myqueue.front();
+        myqueue.pop();
+    }
+}
+
+void printSequences(string &filePath, int length){
+    string word;
+    ifstream myfile;
+    myfile.open(filePath);
+    queue<string> q;
+    int wordCnt = 0;
+    if(myfile.is_open()){
+        cout << "file open" << endl;
+        char c;
+        while(myfile.get(c)){
+            if(c != ' '){
+                //ignore special charcters
+                if(c < 46 || c > 122){
+                    //ignoring special characters
+                }
+                else{
+                    word+= c;
+                }
+            }
+            else if( c == ' '){
+                wordCnt++;
+                q.push(word);
+                if(wordCnt >=6){
+                    printQ(q);
+                    q.pop();
+                    cout << endl;
+                }
+                word = "";
+            }
+        }
+    }
+    else{
+        cout << "file not open" << endl;
+    }
+}
+
 
 int main(int argc, char *argv[]) {
     std::cout << "cheaters!" << std::endl;
@@ -31,6 +76,13 @@ int main(int argc, char *argv[]) {
     for (unsigned int i = 0;i < files.size();i++) {
         cout << i <<": " << files[i] << endl;
     }
+    stringstream ss;
+    ss << "sm_doc_set/" << files[2];
+    string filePath;
+    ss >> filePath;
+    cout << endl;
+    printSequences(filePath, 6);
+
 
     return 0;
 }
