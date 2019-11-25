6893219d7ca4a918a2ac6c5835700bde1842ecec
ryancdavis5 <47760457+ryancdavis5@users.noreply.github.com>
2019-11-25 11:19:11 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/cheaters.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cheaters.dir/main.cpp.obj"
+  "cheaters.pdb"
+  "cheaters.exe"
+  "cheaters.exe.manifest"
+  "libcheaters.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cheaters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "cheaters!" << std::endl;
+    return 0;
+}
