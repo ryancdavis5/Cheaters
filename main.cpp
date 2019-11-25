e5e7003bc37480dba283469b62e5f4931263a76a
ryancdavis5 <47760457+ryancdavis5@users.noreply.github.com>
2019-11-25 14:31:13 -0600

    next commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <string>
 
-int main() {
+using namespace std;
+int getdir (string dir, vector<string> &files){
+    DIR *dp;
+    struct dirent *dirp;
+    if((dp  = opendir(dir.c_str())) == NULL) {
+        cout << "Error(" << errno << ") opening " << dir << endl;
+        return errno;
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        files.push_back(string(dirp->d_name));
+    }
+    closedir(dp);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
     std::cout << "cheaters!" << std::endl;
+    // must change to use program args at some point
+    string dir = string("sm_doc_set");
+    vector<string> files = vector<string>();
+    getdir(dir,files);
+    for (unsigned int i = 0;i < files.size();i++) {
+        cout << i <<": " << files[i] << endl;
+    }
+
     return 0;
 }
